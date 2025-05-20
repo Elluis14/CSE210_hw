bd891160729a1331327a09d82367a2bb434a6654
Elluis14 <95771375+Elluis14@users.noreply.github.com>
2025-05-19 21:46:31 -0700

    Second Commit

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -11,7 +11,7 @@ class Program
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
-        // Parte 1: Recolección de números
+        
         while (userNumber != 0)
         {
             Console.Write("Enter number: ");
@@ -24,7 +24,7 @@ class Program
             }
         }
 
-        // Parte 2: Calcular la suma
+       
         int sum = 0;
         foreach (int number in numbers)
         {
@@ -33,11 +33,9 @@ class Program
 
         Console.WriteLine($"The sum is: {sum}");
 
-        // Parte 3: Calcular el promedio
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        // Parte 4: Encontrar el número más grande
         int max = numbers[0];
 
         foreach (int number in numbers)
@@ -50,7 +48,7 @@ class Program
 
         Console.WriteLine($"The largest number is: {max}");
 
-        // --- Stretch Challenge 1: El menor número positivo ---
+        
         int smallestPositive = int.MaxValue;
         bool foundPositive = false;
 
@@ -68,7 +66,6 @@ class Program
             Console.WriteLine($"The smallest positive number is: {smallestPositive}");
         }
 
-        // --- Stretch Challenge 2: Ordenar y mostrar la lista ---
         numbers.Sort();
 
         Console.WriteLine("The sorted list is:");
